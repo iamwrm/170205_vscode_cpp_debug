3aceebf008d2d001c2eebc6e67f2723fc47ddf21
Ren Wang <iamwrm@gmail.com>
2017-02-05 15:15:19 +0800

    测试完成，非常好，可以debug非常方便

---
+++ Debug/CMakeFiles/TEST.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TEST.dir/Users/wangren/Github/170205_temp/t1.cpp.o"
+  "TEST.pdb"
+  "TEST"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TEST.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- t1.cpp
+++ t1.cpp
@@ -1,9 +1,17 @@
+// OSX
+//cmake -DCMAKE_BUILD_TYPE=Debug && make && ./TEST
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
     int a;
     std::cout << "hehehehehf";
+    string s1;
+    s1 = "\nabscs\n";
+    cout << s1;
     int b = 1;
     int c = 2;
     return 0;
